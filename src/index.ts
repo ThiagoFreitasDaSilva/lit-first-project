79245c8af5645a5063f1751edb1c7314b0748797
Thiago Freitas da Silva <thiago.silva198@fatec.sp.gov.br>
2024-12-27 11:49:28 -0300

    primeiro commit projeto lit

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { LitFirstProject } from './LitFirstProject.js';
---
+++ src/lit-first-project.ts
@@ -0,0 +1,3 @@
+import { LitFirstProject } from './LitFirstProject.js';
+
+window.customElements.define('lit-first-project', LitFirstProject);
